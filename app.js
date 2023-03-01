52251516e974b92e03bf5d01ced380dde158ab20
Arya Kumar <ar.kr.031@gmail.com>
2023-03-01 10:55:40 +0530

    updated app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Apple API');
 });
+app.get('/app2', function (req, res) {
+  res.send('Apple API path /app2');
+});
 app.listen(6003, function () {
   console.log('Example app listening on port 6003!');
 });
