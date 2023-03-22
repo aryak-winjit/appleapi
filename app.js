beebff5d234e7afc4aa884464e053f98c3ee52a5
Arya Kumar <ar.kr.031@gmail.com>
2023-03-22 16:45:53 +0530

    secret added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
+const fs = require('fs');
+
 var express = require('express');
 var app = express();
 var env_data_owner = process.env.OWNER;
+
+
 app.get('/', function (req, res) {
   res.send('Banana API!');
 });
@@ -15,6 +19,18 @@ app.get('/banana', function (req, res) {
 app.get('/v2apple', function (req, res) {
   res.send('/apple/v2apple ' + process.env.VERSION + " " + process.env.DB_USER + " " + process.env.DB_PASS);
 });
+
+
+
+
+fs.readFile('./secret/secret.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
 
 app.listen(6003, function () {
   console.log('appleapi listening on port 6003!' + process.env.VERSION + " " + process.env.DB_USER + " " + process.env.DB_PASS);
